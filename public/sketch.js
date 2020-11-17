a5f24ff9956e01ba7ce68b89f9ad24708768e287
David Friesen <david.t.friesen@gmail.com>
2020-11-17 08:43:47 -0800

    Add files via upload

---
+++ public/sketch.js
@@ -0,0 +1,79 @@
+const socket = io.connect();
+
+socket.on('connect', () => {
+    console.log('client connected')
+})
+
+
+let env, osc;
+let myCircle;
+
+
+
+
+function setup(){
+    createCanvas(600, 600);
+    
+    env = new p5.Envelope(0.01, 0.7, 0.3, 0.0);
+    osc = new p5.Oscillator('sine');
+    osc.start();
+    osc.amp(0);
+
+
+    const myCircleOpts = {
+        x: random(50, width-50),
+        y: random(50, height-50),
+        size: random(20,40),
+        col: [random(255), random(255), random(255)],
+    }
+
+
+    myCircle = new SoundCircle(socket.id, myCircleOpts.x, myCircleOpts.y, myCircleOpts.size, myCircleOpts.col);
+}
+
+const data = {
+    x: myCircle.pos.x,
+    y: myCircle.pos.y,
+    size: myCircle.size,
+    col: myCircle.col,
+    clicked: myCircle.clicked,
+}
+
+socket.emit('start', data);
+
+function draw(){
+    background(120,90,200);
+
+    const data = {
+        x: myCircle.pos.x,
+        y: myCircle.pos.y,
+        size: myCircle.size,
+        col: myCircle.col,
+        clicked: myCircle.clicked,
+    }
+
+    socket.emit('update', data);
+
+    myCircle.move();
+    myCircle.checkEdges();
+    myCircle.display();
+}
+
+
+function mousePressed(){
+    const playSound = myCircle.checkClick(mouseX, mouseY);
+    if(playSound){
+        const freq = myCircle.size * 10;
+        playSound(freq);
+    }
+}
+
+function mouseReleased(){
+    myCircle.clicked = false;
+    myCircle.setSpeed(mouseX, mouseY);
+}
+
+function playSound(freq){
+    osc.freq(freq);
+    env.play(osc);
+}
